d0e6293cef8a27ad248b8d6e79e1c54a85855a5f
Nauman Haider <naumanhaider868@gmail.com>
2022-12-09 16:32:44 +0500

    Admin Login

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Dashboard from './Componets/Dashboard';
+import Login from './Componets/Login';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Dashboard/> */}
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
